7ab054e6b51cc38c661043ee580409c981a246b4
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-06-08 10:57:49 -0300

    init

---
+++ src/main/java/app/entity/ShoppingCart.java
@@ -0,0 +1,37 @@
+package app.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+
+    private List<Item> items;
+
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+    }
+
+    public double calculateTotal() {
+        double total = 0;
+        for (Item item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public int getNumberOfItems() {
+        return items.size();
+    }
+
+    public boolean containsItem(Item item) {
+        return items.contains(item);
+    }
+}
